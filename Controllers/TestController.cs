1008e61f6957b4e56cb8c7d0412d2d9237fd4ff8
neethu2016mca <37532736+neethu2016mca@users.noreply.github.com>
2019-02-28 16:43:54 +0530

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dailytask.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getStudent() 
+        {
+            return View();
+        }
+	}
+}
---
+++ Models/NewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace dailytask.Models
+{
+    public class NewModel
+    {
+        [Required(ErrorMessage = "Mandantory")]
+        [Display(Name = "Student Name")]
+        public string Sname { get; set; }
+        [Required(ErrorMessage = "Mandantory")]
+        [Display(Name = "Student Address")]
+        [DataType(DataType.MultilineText)]
+        public string address { get; set; }
+        public Gender genderList { get; set; }
+        public Course CourseList { get; set;}
+        [Required(ErrorMessage = "Mandantory")]
+        [Display(Name = "Student E-Mail")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Mandantory")]
+        [Display(Name = "Student User Name")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Mandantory")]
+        [Display(Name = "Student Password")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        [Required(ErrorMessage = "Mandantory")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string confirmPassword { get; set; }
+    }
+
+    public enum Course
+    {
+        BTech,
+        MTech,
+        MCA
+    }
+}
